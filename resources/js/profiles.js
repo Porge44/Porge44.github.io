861316806bd80f9bc5309a16decc7a1c93788676
Georgia <70899556+Porge44@users.noreply.github.com>
2021-07-02 11:33:41 +0800

    fixed the stupid mistake, tink should open now

--- resources/js/profiles.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    // Get the id of the clicked list thing
-    var name = $(".chara").on("click", function () {
-        $(".chara").attr("id"));
-});
-
-// Toggle the relevant profile's visibility
-$(name).toggle();
-});
---
+++ resources/js/tink.js
@@ -0,0 +1,3 @@
+function openTink() {
+    window.open("tinkerhell.html", "_blank", "width=580,height=400,toolbar=0,scrollbar=0");
+}
