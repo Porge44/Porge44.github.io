0442197f8170b1a373fb31ac9fe5dddfd7916f1a
Georgia <70899556+Porge44@users.noreply.github.com>
2021-07-02 11:27:21 +0800

    added tink

--- resources/js/isaiah.js
+++ resources/js/isaiah.js
@@ -1,3 +1,8 @@
 function openWindow() {
     window.open("isaiah.html", "_blank", "width=580,height=400,toolbar=0,scrollbar=0");
 }
+
+function openTink() {
+
+    window.open("tinkerhell.html", "_blank", "width=580,height=400,toolbar=0,scrollbar=0");
+}
